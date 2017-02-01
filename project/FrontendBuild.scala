aa1f22a34890e03cbf19705bb17475f5f9a7ca7a
Periklis <periklas@hotmail.com>
2017-02-01 11:45:19 +0000

    DDCNLS-628: Removed commented libraries.

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -1,12 +1,8 @@
 
 import com.typesafe.sbt.web.SbtWeb
-import scoverage.ScoverageSbtPlugin
 import uk.gov.hmrc.SbtAutoBuildPlugin
 import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
 import uk.gov.hmrc.versioning.SbtGitVersioning
-import play.sbt.routes.RoutesKeys.routesGenerator
-import play.sbt.routes.RoutesKeys._
-//import play.sbt.PlayImport.PlayKeys._
 import com.typesafe.sbt.web.SbtWeb.autoImport._
 import sbt.Keys._
 import sbt._
@@ -41,43 +37,34 @@ private object AppDependencies {
 
   private val frontendBootstrapVersion = "7.10.0"
   private val govukTemplateVersion =  "5.0.0"
-  //private val httpVerbsVersion = "6.2.0"
   private val playAuthorisedFrontendVersion = "6.2.0"
   private val playConfigVersion = "3.0.0"
   private val playHealthVersion = "2.0.0"
-  //private val playJsonLogger = "3.0.0"
   private val playPartials = "5.2.0"
-  private val playUIVersion = "5.1.0"
+  private val playUIVersion = "5.3.0"
   private val urlBuilderVersion = "1.1.0"
-  //private val scalaTestPlusVersion = "1.2.0"
   private val scalaTestVersion = "2.2.6"
   private val hmrcTestVersion = "2.1.0"
   private val mockitoVersion = "1.10.19"
   private val specs2Version = "2.3.13"
   private val pegDownVersion = "1.6.0"
   private val jSoupVersion = "1.9.2"
-  //private val metricsGraphiteVersion = "3.0.2"
-  //private val metricsPlayVersion = "0.2.1"
   private val playGraphiteVersion = "3.1.0"
+  private val logbackJsonLoggerVersion = "3.1.0"
+  private val scalatestPlusPlayVersion = "1.5.1"
 
   val compile = Seq(
 
-    "uk.gov.hmrc" %% "frontend-bootstrap" % frontendBootstrapVersion, // includes the global object and error handling, as well as the FrontendController classes
+    "uk.gov.hmrc" %% "frontend-bootstrap" % frontendBootstrapVersion,
     "uk.gov.hmrc" %% "govuk-template" % govukTemplateVersion,
-    //"uk.gov.hmrc" %% "http-verbs" % httpVerbsVersion,
-    "uk.gov.hmrc" %% "play-authorised-frontend" % playAuthorisedFrontendVersion, // use when your frontend requires authentication
-    "uk.gov.hmrc" %% "play-config" % playConfigVersion, // includes helper classes for retrieving Play configuration
+    "uk.gov.hmrc" %% "play-authorised-frontend" % playAuthorisedFrontendVersion,
+    "uk.gov.hmrc" %% "play-config" % playConfigVersion,
     "uk.gov.hmrc" %% "play-health" % playHealthVersion,
-    //"uk.gov.hmrc" %% "play-json-logger" % playJsonLogger,
-    "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
-    "uk.gov.hmrc" %% "play-partials" % playPartials, // includes code for retrieving partials, e.g. the Help with this page form
+    "uk.gov.hmrc" %% "logback-json-logger" % logbackJsonLoggerVersion,
+    "uk.gov.hmrc" %% "play-partials" % playPartials,
     "uk.gov.hmrc" %% "play-ui" % playUIVersion,
     "uk.gov.hmrc" %% "url-builder" % urlBuilderVersion,
     "uk.gov.hmrc" %% "play-graphite" % playGraphiteVersion
-    //"com.codahale.metrics" % "metrics-graphite" % metricsGraphiteVersion,
-    //"de.threedimensions" %% "metrics-play" % "2.5.13"
-    //"com.typesafe.play" %% "play-ws" % "2.5.8"
-    //"com.kenshoo" %% "metrics-play" % metricsPlayVersion
   )
 
   trait TestDependencies {
@@ -90,17 +77,15 @@ private object AppDependencies {
       override lazy val test = Seq(
         "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
         "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
-        //"org.scalatestplus" %% "play" % scalaTestPlusVersion % scope,
         "org.pegdown" % "pegdown" % pegDownVersion % scope,
         "org.mockito" % "mockito-all" % mockitoVersion,
         "org.specs2" % "specs2_2.10" % specs2Version,
         "org.jsoup" % "jsoup" % jSoupVersion % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-        "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1"
+        "org.scalatestplus.play" %% "scalatestplus-play" % scalatestPlusPlayVersion % scope
       )
     }.test
   }
 
   def apply() = compile ++ Test()
-
 }
