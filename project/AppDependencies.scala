4fad16b9228a8fbe2a4c114d7895e512135d168c
manpreetsingh1000 <39770532+manpreetsingh1000@users.noreply.github.com>
2021-09-14 12:47:04 +0100

    DDCE-2346:Play-frontend-hmrc Upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,16 +9,16 @@ object AppDependencies {
     "uk.gov.hmrc"     %% "url-builder"                  % "3.5.0-play-28",
     "uk.gov.hmrc"     %% "tax-year"                     % "1.4.0",
     "uk.gov.hmrc"     %% "bootstrap-frontend-play-28"   % "5.8.0",
-    "uk.gov.hmrc"     %% "play-frontend-hmrc"           % "0.88.0-play-28",
+    "uk.gov.hmrc"     %% "play-frontend-hmrc"           % "1.4.0-play-28",
     "uk.gov.hmrc"     %% "http-caching-client"          % "9.5.0-play-28",
     compilerPlugin("com.github.ghik" % "silencer-plugin" % silencerVersion cross CrossVersion.full),
     "com.github.ghik" % "silencer-lib" % silencerVersion % Provided cross CrossVersion.full
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.mockito"               % "mockito-core"          % "3.11.0",
-    "org.specs2"                %% "specs2-core"          % "4.12.3",
-    "org.jsoup"                 % "jsoup"                 % "1.14.1",
+    "org.mockito"               % "mockito-core"          % "3.11.2",
+    "org.specs2"                %% "specs2-core"          % "4.12.12",
+    "org.jsoup"                 % "jsoup"                 % "1.14.2",
     "org.scalatestplus.play"    %% "scalatestplus-play"   % "5.1.0",
     "org.scalatestplus"         %% "mockito-3-4"          % "3.2.9.0",
     "com.vladsch.flexmark"      % "flexmark-all"          % "0.35.10",
