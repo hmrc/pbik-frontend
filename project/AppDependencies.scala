c216f1dd74a620298082a2b1bdd98f87631f6bbd
EdCope <87376121+EdCope@users.noreply.github.com>
2023-02-13 16:13:09 +0000

    DDCE-3681: play-frontend-hmrc upgrade and remove url-builder dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,10 +6,9 @@ object AppDependencies {
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "play-partials"              % "8.3.0-play-28",
-    "uk.gov.hmrc" %% "url-builder"                % "3.7.0-play-28",
     "uk.gov.hmrc" %% "tax-year"                   % "3.0.0",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapPlayVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "3.33.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "6.4.0-play-28",
     "uk.gov.hmrc" %% "http-caching-client"        % "10.0.0-play-28"
   )
 
