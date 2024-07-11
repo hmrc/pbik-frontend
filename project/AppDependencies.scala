a49db23777863e2c8c7cdd7476cf019f06d47c56
“Taiwo-AB” <63714619+Taiwo-AB@users.noreply.github.com>
2024-07-11 08:43:17 +0100

    DDCE-5460 Fix warning text issues

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,20 +3,20 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapPlayVersion = "8.6.0"
-  private val hmrcMongoPlayVersion = "2.0.0"
+  private val hmrcMongoPlayVersion = "2.1.0"
 
   private val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "tax-year"                   % "4.0.0",
+    "uk.gov.hmrc"       %% "tax-year"                   % "5.0.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "10.1.0",
-    "uk.gov.hmrc"       %% "domain-play-30"             % "9.0.0"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "10.3.0",
+    "uk.gov.hmrc"       %% "domain-play-30"             % "10.0.0"
   )
 
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoPlayVersion,
-    "org.jsoup"          % "jsoup"                   % "1.17.2"
+    "org.jsoup"          % "jsoup"                   % "1.18.1"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID]      = compile ++ test
