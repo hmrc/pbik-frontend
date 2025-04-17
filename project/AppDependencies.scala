3bc3c7c945525816169ff422eb5369bc87494190
SachaMcCormack <175815565+SachaMcCormack@users.noreply.github.com>
2025-04-17 12:35:46 +0100

    updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,11 +6,11 @@ object AppDependencies {
   private val hmrcMongoPlayVersion = "2.6.0"
 
   private val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "tax-year"                   % "5.0.0",
+    "uk.gov.hmrc"       %% "tax-year"                   % "6.0.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.0.0",
-    "uk.gov.hmrc"       %% "domain-play-30"             % "10.0.0"
+    "uk.gov.hmrc"       %% "domain-play-30"             % "11.0.0"
   )
 
   private val test: Seq[ModuleID] = Seq(
