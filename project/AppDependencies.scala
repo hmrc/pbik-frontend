790c9b03100f9baaa22ec59c8926462912365d77
Suraj Suseelan <186935751+suraj-suseelan@users.noreply.github.com>
2025-08-28 16:58:57 +0100

    DDCE-6866:pbik-frontend - Implement GOV.UK Rebrand (#305)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapPlayVersion = "9.19.0"
+  private val bootstrapPlayVersion = "10.1.0"
   private val hmrcMongoPlayVersion = "2.7.0"
 
   private val compile: Seq[ModuleID] = Seq(
@@ -10,13 +10,13 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.8.0",
-    "uk.gov.hmrc"       %% "domain-play-30"             % "11.0.0"
+    "uk.gov.hmrc"       %% "domain-play-30"             % "13.0.0"
   )
 
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoPlayVersion,
-    "org.jsoup"          % "jsoup"                   % "1.21.1"
+    "org.jsoup"          % "jsoup"                   % "1.21.2"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
